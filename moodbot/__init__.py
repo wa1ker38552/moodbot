c3a26be8ecec9b2de7b6d6ef0d050ef612989b4d
walker <100868154+wa1ker38552@users.noreply.github.com>
2022-09-25 04:57:34 -0700

    Update __init__.py

--- moodbot/__init__.py
+++ moodbot/__init__.py
@@ -1,4 +1,6 @@
 from .message import message
 from .chatbot import chatbot
 from .return_data import output
+from .moodanalyzer import FindMood
 from .discord import discord_trainer
+from .googlechat import googlechat_trainer
